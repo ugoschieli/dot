3ceb21571d70aa218cead6188b46ae52b43eecc2
Ugo Schieli <ugo.schieli@gmail.com>
2025-07-23 17:17:18 +0200

    use local neovim flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    myneovim.url = "github:/ugoschieli/neovim";
+    myneovim.url = "git+file:./neovim";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
